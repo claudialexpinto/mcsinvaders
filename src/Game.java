3b00e686150b824d7cf7b7b531817947505be25a
fabiomartins <fabio.stmartins.09@gmail.com>
2020-02-13 09:22:57 -0100

    Some changes I do not remember well but nevermind 'cause we did it all afterwards in Claudias' branch

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,9 @@
 import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
-public class Game {
+public class Game implements KeyboardHandler {
 
     private Player player;
     private Field field;
@@ -8,15 +11,41 @@ public class Game {
     private Enemy2 enemy2;
     private Enemy3 enemy3;
     private Enemy4 enemy4;
+    private Keyboard keyboard;
 
     public Game(double width, double height) {
         this.field = new Field(width, height);
-        this.enemy = new Enemy(3, field, 700, 50);
-        this.enemy2 = new Enemy2(3, field, 700, 50);
+        this.enemy = new Enemy(5, field, 700, 50);
+        this.enemy2 = new Enemy2(10, field, 700, 50);
         this.player = new Player(field);
-        this.enemy3 = new Enemy3(3, field, 700, 50);
-        this.enemy4 = new Enemy4(3, field, 700, 50);
+        this.enemy3 = new Enemy3(15, field, 700, 50);
+        this.enemy4 = new Enemy4(20, field, 700, 50);
+        this.keyboard = new Keyboard(this);
     }
+
+    public void restart(){
+        KeyboardEvent start = new KeyboardEvent();
+        start.setKey(KeyboardEvent.KEY_R);
+        start.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(start);
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        if (keyboardEvent.getKey() == KeyboardEvent.KEY_R) {
+            try {
+                start();
+            } catch (InterruptedException ex){
+                System.out.println("Restart not possible");
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent){}
+
+
+
 
     public void start() throws InterruptedException {
 
@@ -32,8 +61,15 @@ public class Game {
     public void init() {
 
         field.init();
-
-        enemy.start();
+        if (enemy.getHealth() > 0) {
+            enemy.start();
+        } else if (enemy2.getHealth() > 0) {
+            enemy2.start();
+        } else if (enemy3.getHealth() > 0) {
+            enemy3.start();
+        } else {
+            restart();
+        }
 
         collisionDetector(player, enemy);
     }
--- src/MainTester.java
+++ src/MainTester.java
@@ -1,4 +1,10 @@
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
 public class MainTester {
+
     public static void main(String[] args) throws InterruptedException{
         Game g = new Game(1489,838);
 
